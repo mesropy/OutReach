3ca2e95ba2a4789ab956fd72e4f9241ef1f7dcf6
jamilfa1 <faisal.jamil@mail.utoronto.ca>
2020-06-23 03:44:36 -0400

    Registration page functionality completed

--- src/react-components/Register/RegisterForm/index.js
+++ src/react-components/Register/RegisterForm/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { uid } from "react-uid";
+import {Tooltip, OverlayTrigger} from "react-bootstrap"
 
 class RegisterForm extends React.Component {
     constructor(props) {
@@ -7,21 +9,45 @@ class RegisterForm extends React.Component {
         this.years = Array.from(new Array(100), (val, index) => year - index)
     }
 
+
     render() {
+        const {
+            username,
+            password,
+            confirmPassword,
+            phoneNumber,
+            city,
+            age,
+            handleChange,
+            handleSubmit
+        } = this.props;
+
         return (
             <div>
                 <h6>already have an account? <a href="/">login</a></h6>
                 <form>
-                    <input type="text" placeholder="username"></input><br/>
-                    <input type="text" placeholder="password"></input><br/>
-                    <input type="text" placeholder="confirm password"></input><br/>
-                    <input type="text" placeholder="phone number"></input><br/>
-                    <select name="city">
+                    *<OverlayTrigger placement="right" overlay= {
+                        <Tooltip>Enter a combination of atleast six letters and numbers</Tooltip>
+                    }><input type="text" name="username" value={username} placeholder="username" onChange={handleChange}></input></OverlayTrigger><br/>
+                    *<OverlayTrigger placement="right" overlay= {
+                        <Tooltip>Enter a combination of atleast six letters, numbers, and punctuation marks</Tooltip>
+                    }><input type="password" name="password" value={password} placeholder="password" onChange={handleChange}></input></OverlayTrigger><br/>
+                    *<input type="password" name="confirmPassword" value={confirmPassword} placeholder="confirm password" onChange={handleChange}></input><br/>
+                    *<input type="text" name="phoneNumber" value={phoneNumber} placeholder="phone number" onChange={handleChange}></input><br/>
+                    *<select name="city" value={city} onChange={handleChange}>
+                        <option disabled value="default">select a city</option>
                         <option value="Toronto">Toronto</option>
                     </select><br/>
-                    <input type="text" placeholder="select age (optional)"></input><br/>
-                    <input type="submit" value="sign-up"></input>
+                    <select name="age" value={age} onChange={handleChange}>
+                        <option disabled value="default">select age (optional)</option>
+                        {
+                            this.years.map((year, index) => {
+                                return (<option key={uid({year})} value={year}>{year}</option>)
+                            })
+                        }
+                    </select><br/>
                 </form>
+                <button type="submit" onClick={handleSubmit} aria-label="Hi" data-balloon-pos="up">sign-up</button>
             </div>
         )           
     }
--- src/react-components/Register/index.js
+++ src/react-components/Register/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import '../main_styles.css'
+import {Redirect} from 'react-router-dom';
+import '../main_styles.css';
 import './styles.css';
 
 import RegisterForm from './RegisterForm'
@@ -11,15 +12,21 @@ class Register extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      register: false,
       username: "",
       password: "",
       confirmPassword: "",
       phoneNumber: "",
-      city: "",
-      age: ""
+      city: "default",
+      age: "default"
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.checkEmpty = this.checkEmpty.bind(this);
+    this.checkUsername = this.checkUsername.bind(this);
+    this.checkPassword = this.checkPassword.bind(this);
+    this.checkPhone = this.checkPhone.bind(this);
+    this.checkCity = this.checkCity.bind(this);
   }
 
   handleChange(e) {
@@ -30,15 +37,71 @@ class Register extends React.Component {
     })
   }
 
+  checkEmpty() {
+    return (this.state.username === "" || this.state.password === "" || this.state.confirmPassword === "" || this.state.phoneNumber === "" || this.state.city === "default")
+  }
+
+  checkUsername() {
+    return !(this.state.username.length >= 6 && /^[0-9a-zA-Z]+$/.test(this.state.username))
+  }
+
+  checkPassword() {
+    return !(this.state.password.length >= 6 && /\d/.test(this.state.password) && /[a-zA-Z]/.test(this.state.password) && /[special_characters]/.test(this.state.password))
+  }
+
+  checkPhone() {
+    return !(/^\d{10}$/.test(this.state.phoneNumber))
+  }
+
+  checkCity() {
+    return !(this.state.city === "Toronto")
+  }
+
   handleSubmit(e) {
-    console.log(e.target.value);
+    if (this.checkEmpty()) {
+      alert("Please fill in all required fields.");
+    }
+    else if (this.checkUsername()) {
+      alert("Username must match the required description.")
+    }
+    else if (this.checkPassword()) {
+      alert("Password must match the required description.");
+    }
+    else if (this.state.password !== this.state.confirmPassword) {
+      alert("Confirm Password must match the Password entered.");
+    }
+    else if (this.checkPhone()) {
+      alert("Please enter a valid Phone Number.");
+    }
+    else if (this.checkCity()) {
+      alert("Please select a valid city.");
+    }
+    else {
+      alert("Registration Success!");
+      this.setState({
+        register: true
+      })
+    }
   }
 
   render() {
+    if (this.state.register === true) {
+      return <Redirect to='/'/>
+    }
+
     return (
       <div>
-        <LeftSideHeader></LeftSideHeader>
-        <RegisterForm></RegisterForm>
+        <LeftSideHeader/>
+        <RegisterForm
+          username={this.state.username}
+          password={this.state.password}
+          confirmPassword={this.state.confirmPassword}
+          phoneNumber={this.state.phoneNumber}
+          city={this.state.city}
+          age={this.state.age}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
       </div>
     );
   }
