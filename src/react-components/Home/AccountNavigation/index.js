973fd3f53e33e10c01ac82ba2f5d3d7f0a43d285
Lilit Mesropyan <lilit.mesropyan@gmail.com>
2020-06-26 16:26:49 -0400

    Change account navigation based on whether or not user is logged in

--- src/react-components/Home/AccountNavigation/index.js
+++ src/react-components/Home/AccountNavigation/index.js
@@ -8,29 +8,73 @@ import "./styles.css";
 // or username and logout buttons
 // (depending on whether the user is logged in or not)
 class AccountNavigation extends React.Component{
-  state = {
-    loggedIn: false
+  constructor(props) {
+    super(props);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    // TODO: get these from App.js as props
+    this.state = {
+      isLoggedIn: true,
+      isAdmin: false,
+      username: "username"
+    }
+  }
+
+  handleLogout() {
+    this.setState({
+      isLoggedIn: false
+    });
+  }
+
+  handleLogin(){
+    this.setState({
+      isLoggedIn: true
+    });
   }
 
   render () {
-    // get username prop
-    /*const { username } = this.props*/
+    const renderButtons = () => {
+      if (this.state.isLoggedIn){
+        return (
+          <div className="account_nav">
+            {/* TODO: add path to account */}
+            <Button component= { Link } to={"/Admin"}
+                    className="username_button"
+                    disableRipple
+                    variant="text">
+              {/* TODO: add profile icon */}
+              { this.state.username }
+            <div className="divider"/>
+            </Button>
+            {/* TODO: add onclick function that logs out user */}
+            <Button onClick={ this.handleLogout }
+                    className="account_button"
+                    variant="outlined"
+                    color="primary">
+              Logout
+            </Button>
+          </div>
+        );
+      } else {
+        return (
+          <ButtonGroup className="account_nav"
+                       variant= "outlined"
+                       color="primary" >
+             {/* TODO: add path to login page */}
+              <Button onClick={ this.handleLogin }
+                      className="account_button">
+                      login
+              </Button>
+              <Button component= { Link } to={"./../Register"}
+                      className="account_button">
+                      sign-up
+              </Button>
+          </ButtonGroup>
+        );
+      }
+    }
 
-    return(
-      <ButtonGroup className="account_nav"
-                   variant= "outlined"
-                   color="primary" >
-         {/* TODO: add path to login page */}
-          <Button component= { Link } to={"/Admin"}
-                  className="account_button">
-                  Login
-          </Button>
-          <Button component= { Link } to={"./../Register"}
-                  className="account_button">
-                  Sign-Up
-          </Button>
-      </ButtonGroup>
-    );
+    return renderButtons();
   }
 }
 
