b197ac1e0c47c7333846aa4985a49ed823fec0d1
Ya-Tzu Wang <yacat24@gmail.com>
2020-06-30 17:41:10 -0400

    added time & location button to post, change layout of post based on prototype

--- src/react-components/Toronto/Post/index.js
+++ src/react-components/Toronto/Post/index.js
@@ -1,21 +1,33 @@
 import React from "react";
 import './styles.css';
-import profilePicture from '../static/profile_picture.png';
+// import profilePicture from '../static/profile_picture.png';
+import PinIcon from '../../Home/MapSelection/Pin/PinIcon'
 
 class Post extends React.Component {
 
     render() {
-        const {content, username="user"} = this.props;
+        const {content, username="user", time="12:34 pm·Today",} = this.props;
 
         return (
             <div className="post">
 
-                <div className="iconContainer">
+                {/* <div className="iconContainer">
                     <img src={profilePicture} alt="userIcon"/>
+                </div> */}
+
+                <div className="post-bar">
+                    <p className="post-username"><strong>{username}</strong></p>
+                    {/* <p><strong>{age}</strong></p> */}
+                    <div className="post-rightBar">
+                        <button className="pin_button">
+                            <PinIcon />
+                        </button>
+                        <p>{time}</p>
+                    </div>
                 </div>
 
                 <div className="postContent">
-                    <p><strong>{username}</strong></p>
+                    {/* <p><strong>{username}</strong></p> */}
                     <p>{content}</p>
                 </div>
 
--- src/react-components/Toronto/Queue/index.js
+++ src/react-components/Toronto/Queue/index.js
@@ -8,8 +8,9 @@ class Queue extends React.Component {
     state = {
         name: "user",
         content: "",
+        time: "12:34 pm·Today",
         list: [
-            ["user", "I am graduating in a few months. Worried about job market under covid :("]
+            ["user", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget maximus massa. Vestibulum hendrerit nec urna eu elementum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc vehicula turpis vitae eros convallis, suscipit lobortis neque vestibulum. Morbi ac augue at nisl porttitor varius. Suspendisse elementum tincidunt ullamcorper. Fusce mi arcu, vehicula in facilisis sit amet, eleifend ut sem. Aenean volutpat feugiat nulla vel egestas."]
         ]
     };
 
