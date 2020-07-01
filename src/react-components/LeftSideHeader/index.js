1765022ee1927778ecc8e3f077f2187ffba580c1
jamilfa1 <faisal.jamil@mail.utoronto.ca>
2020-07-01 13:21:43 -0400

    Remove extra scrollbars in registration page

--- src/react-components/LeftSideHeader/index.js
+++ src/react-components/LeftSideHeader/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import logo from './static/logo.svg'
 import './styles.css'
 
--- src/react-components/Register/RegisterForm/index.js
+++ src/react-components/Register/RegisterForm/index.js
@@ -24,31 +24,31 @@ class RegisterForm extends React.Component {
         } = this.props;
 
         return (
-            <div id="register_div">
-                <h6>already have an account? <a href="/Login">login</a></h6>
-                <form id="form">
-                    <span className="star">*</span><OverlayTrigger placement="left" overlay= {
-                        <Tooltip>Enter a combination of atleast six letters and numbers</Tooltip>
-                    }><input type="text" name="username" value={username} placeholder="username" onChange={handleChange}></input></OverlayTrigger><br/>
-                    <span className="star">*</span><OverlayTrigger placement="left" overlay= {
-                        <Tooltip>Enter a combination of atleast six letters, numbers, and punctuation marks</Tooltip>
-                    }><input type="password" name="password" value={password} placeholder="password" onChange={handleChange}></input></OverlayTrigger><br/>
-                    <span className="star">*</span><input type="password" name="confirmPassword" value={confirmPassword} placeholder="confirm password" onChange={handleChange}></input><br/>
-                    <span className="star">*</span><input type="number" name="phoneNumber" value={phoneNumber} placeholder="phone number" onChange={handleChange}></input><br/>
-                    <span className="star">*</span><select name="city" value={city} onChange={handleChange}>
-                        <option disabled value="default">select a city</option>
-                        <option value="Toronto">Toronto</option>
-                    </select><br/>
-                    <select id="age_option" name="age" value={age} onChange={handleChange}>
-                        <option disabled value="default">select age (optional)</option>
-                        {
-                            this.years.map((year, index) => {
-                                return (<option key={uid({year})} value={year}>{year}</option>)
-                            })
-                        }
-                    </select><br/>
+            <div id="register_div" className="text-center">
+                <form id="register_form">
+                    <h6 className="register_info">already have an account? <a href="/Login">login</a></h6>
+                        <span className="star">*</span><OverlayTrigger placement="left" overlay= {
+                            <Tooltip>Enter a combination of atleast six letters and numbers</Tooltip>
+                        }><input className="register_input" type="text" name="username" value={username} placeholder="username" onChange={handleChange}></input></OverlayTrigger><br/>
+                        <span className="star">*</span><OverlayTrigger placement="left" overlay= {
+                            <Tooltip>Enter a combination of atleast six letters, numbers, and punctuation marks</Tooltip>
+                        }><input className="register_input" type="password" name="password" value={password} placeholder="password" onChange={handleChange}></input></OverlayTrigger><br/>
+                        <span className="star">*</span><input className="register_input" type="password" name="confirmPassword" value={confirmPassword} placeholder="confirm password" onChange={handleChange}></input><br/>
+                        <span className="star">*</span><input className="register_input" type="number" name="phoneNumber" value={phoneNumber} placeholder="phone number" onChange={handleChange}></input><br/>
+                        <span className="star">*</span><select className="register_select" name="city" value={city} onChange={handleChange}>
+                            <option disabled value="default">select a city</option>
+                            <option value="Toronto">Toronto</option>
+                        </select><br/>
+                        <select id="age_option" className= "register_select" name="age" value={age} onChange={handleChange}>
+                            <option disabled value="default">select age (optional)</option>
+                            {
+                                this.years.map((year, index) => {
+                                    return (<option key={uid({year})} value={year}>{year}</option>)
+                                })
+                            }
+                        </select><br/>
+                        <button id="signup_button" type="button" onClick={handleSubmit}>sign-up</button>
                 </form>
-                <button id="signup" type="submit" onClick={handleSubmit}>sign-up</button>
             </div>
         )           
     }
