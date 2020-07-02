df2a0b234de931a201e6423806e268e10e947699
Lilit Mesropyan <lilit.mesropyan@gmail.com>
2020-07-01 23:33:30 -0400

    Move WorldMapNav from Toronto's TopBar to separate component

---
+++ src/react-components/WorldMapNav/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons'
+
+import "./styles.css";
+
+class WorldMapNav extends React.Component {
+  render(){
+    return(
+      <Link id="worldMapNav" to={'/'}>
+          <FontAwesomeIcon icon={faGlobeAmericas}></FontAwesomeIcon>
+          <h6>World Map</h6>
+      </Link>
+    );
+  }
+}
+
+export default WorldMapNav;
