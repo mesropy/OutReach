51d16235abd295e8610ab514ab621956661e3894
jamilfa1 <faisal.jamil@mail.utoronto.ca>
2020-07-02 16:47:19 -0400

    Implement disapprove, approve, delete from pending/published messages & more CSS

--- src/react-components/Admin/UserMessages/Pending/index.js
+++ src/react-components/Admin/UserMessages/Pending/index.js
@@ -2,18 +2,41 @@ import React from "react";
 import { uid } from "react-uid";
 import './styles.css'
 import Message from "../../../Message"
+import {approveMessage} from "../../../../actions/adminMessagesEdit"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 class Pending extends React.Component {
 
     render() {
-        const pendingMessages = this.props.userMessagesComponent.state.pendingMessages
-        const edit = this.props.edit ?  <div id="edit_div" className="text-center">
-                                            <button id="approve" className="text-center"><FontAwesomeIcon icon={faCheck}></FontAwesomeIcon><p>approve</p></button>
-                                            <button id="disapprove" className="text-center"><FontAwesomeIcon icon={faTimes}></FontAwesomeIcon><p>disapprove</p></button>
-                                        </div> : null
-        const messageClass = this.props.edit ? "pending_post_edit" : "post_div"
+        const {edit, userMessagesComponent, pendingMessages, handlePopup} = this.props
+        const messageClass = edit ? "pending_post_edit" : "post_div"
+        if (edit) {
+            return (
+                <div id="pending_div">
+                    {pendingMessages.map(message => (
+                            <div id="messageContainer">
+                                <div className={messageClass}>
+                                    <Message
+                                        key={uid(message)}
+                                        username={message[0]}
+                                        content={message[1]}
+                                    />
+                                </div>
+                                <div id="edit_div" className="text-center">
+                                    <button id="approve" className="text-center" onClick={() => {approveMessage.bind(this, userMessagesComponent, message)()}}>
+                                        <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon><p>approve</p>
+                                    </button>
+                                    <button id="disapprove" className="text-center" onClick={() => {handlePopup.bind(userMessagesComponent, message)()}}>
+                                        <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon><p>disapprove</p>
+                                    </button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            )
+        }
         return (
             <div id="pending_div">
                 {pendingMessages.map(message => (
@@ -25,7 +48,6 @@ class Pending extends React.Component {
                                     content={message[1]}
                                 />
                             </div>
-                            {edit}
                         </div>
                     ))
                 }
