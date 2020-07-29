183c1c7604314e4be05b189dca47a41143022a38
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-07-29 14:42:48 -0500

    Move server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
