43c8f15af20f0dc6b816884f2c3180756984dee9
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-07-29 15:01:40 -0500

    inital upload

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'src')));
 
 const port = process.env.PORT || 5000
+
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
 }) 
