8a758607ddf8ae381ba3b9d3011a27ef0effdd1b
CIA-PC <faisal.jamil@mail.utoronto.ca>
2020-08-04 15:00:22 -0500

    Add test route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static(path.join(__dirname, 'src')));
 
 const port = process.env.PORT || 5000
 
+app.get('/ping', function (req, res) {
+	return res.send('pong');
+});
+
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
 }) 
