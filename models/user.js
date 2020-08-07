b3875b41ca1516f2c5f876b3a324ffa96aaa67fd
Lilit Mesropyan <lilit.mesropyan@gmail.com>
2020-08-07 15:35:13 -0400

    Update user and message models

--- models/user.js
+++ models/user.js
@@ -6,16 +6,21 @@ const MessageSchema = new mongoose.Schema({
     text: String,
     date: String,
     location: String,
-    published: Boolean
+    published: Boolean,
+    creator: {
+		    type: mongoose.Schema.Types.ObjectId,
+		      required: true
+	  }
 });
 
 
 const UserSchema = new mongoose.Schema({
     name: String,
+    password: String,
     dob: String,
     phone: String,
-    public: Boolean,
-    messages: [MessageSchema]
+    public: Boolean
+    // messages: [MessageSchema]
 });
 
 const User = mongoose.model('User', UserSchema);
